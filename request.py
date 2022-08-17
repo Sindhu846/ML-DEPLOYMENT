e1be604e788ad03175a5dae6b7c5322a426c4a14
Sindhu846 <106531907+Sindhu846@users.noreply.github.com>
2022-08-17 17:53:33 +0530

    Add files via upload

---
+++ request.py
@@ -0,0 +1,10 @@
+import requests
+
+url = 'http://localhost:5000/predict_api'
+r = requests.post(url,json={'Hour (0 to 24)':10, 'Temperature (°C)':10, 'Humidity (%)':60,'Wind Speed (m/s)':3,
+                            'Visibility (10m)':1000
+                            ,'Solar radiation (MJ/m2)':0,'Rainfall (mm)':0,'Snowfall (cm)':2,'Month':12
+                            ,'WeekDay (1 to 7)': 1 })
+
+print(r.json())
+
